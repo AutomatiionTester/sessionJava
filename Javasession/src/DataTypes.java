3c339196257b043150a1d5c5e7d0ce78e8c65fd7
sush0 <sp@gmail.com>
2020-09-14 11:13:25 -0400

    First Commit uploading all the files

---
+++ Javasession/src/DataTypes.java
@@ -0,0 +1,46 @@
+
+public class DataTypes {
+	
+	
+
+	public static void main(String[] args) {
+		
+  int i=10;
+  int i1=20;
+  int j=100;
+  int k=10;
+  int p=0;
+  int age=25;
+  System.out.println(i);
+  System.out.println(age);
+  System.out.println(i+j);
+  
+  // double (8bytes),float(4bytes)
+    double d1=12.33;
+    double d2=-12.33;
+    double d3 =100;//100.00
+    System.out.println(d1);
+    
+    //char :
+    char c1 ='a';
+    char c2 ='$';
+    char c3 ='1';
+    char gender ='M';
+    //boolean
+    boolean b1 =true;
+    boolean b2=false;
+    // String: is a class in java
+    String s1="Hello World";
+    String s2="Selenium";
+    String s3= "Hi this is my java code";
+    String s4 ="1000";
+    String s5 = " 12.33";
+    System.out.println(s1);
+    System.out.println(s2);
+    System.out.println(100);
+    System.out.println("HHello World");
+    
+    
+	}
+
+}
